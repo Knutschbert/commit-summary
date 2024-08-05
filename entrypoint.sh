350bc162de5cf2f09e5d40abb6fcfe777682171a
Nihi <134062045+Knutschbert@users.noreply.github.com>
2024-08-05 17:39:21 +0200

    Update entrypoint.sh typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ new_tag=$1
 previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
 commit_summary="$(git log --oneline --pretty=tformat:"%h %s" $previous_tag..$new_tag)"
 
-echo "${summary}<<$EOF" >> $GITHUB_OUTPUT
+echo "summary<<$EOF" >> $GITHUB_OUTPUT
 echo "${commit_summary}" >> $GITHUB_OUTPUT
 echo "$EOF" >> $GITHUB_OUTPUT

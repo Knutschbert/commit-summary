3c86251be3e67c0a084dcc1be3d01b3f40b43c1e
Nihi <134062045+Knutschbert@users.noreply.github.com>
2024-08-05 17:34:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ new_tag=$1
 
 previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
 commit_summary="$(git log --oneline --pretty=tformat:"%h %s" $previous_tag..$new_tag)"
-commit_summary="${commit_summary//'%'/'%25'}"
-commit_summary="${commit_summary//$'\n'/'%0A'}"
-commit_summary="${commit_summary//$'\r'/'%0D'}"
-echo "${summary}=${commit_summary}" >> $GITHUB_OUTPUT
+
+echo "${summary}<<$EOF" >> $GITHUB_OUTPUT
+echo "${commit_summary}" >> $GITHUB_OUTPUT
+echo "$EOF" >> $GITHUB_OUTPUT
